1cbcc6f068b8395821c4ec05070be6a707f66b5d
jjtana <dexydax@gmail.com>
2024-07-23 00:22:54 -0400

    Add project files.

---
+++ Refran/Course.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refran
+{
+    public abstract class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public abstract void Subscribe(Student std);
+    }
+
+    public class OnlineCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            Console.WriteLine($"Student {std.Name} subscribed to online course: {Title}");
+        }
+    }
+
+    public class OfflineCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            Console.WriteLine($"Student {std.Name} subscribed to offline course: {Title}");
+        }
+    }
+
+    public class HybridCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            Console.WriteLine($"Student {std.Name} subscribed to hybrid course: {Title}");
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Refran/Program.cs
@@ -0,0 +1,11 @@
+﻿using Refran;
+
+Student student = new Student { Name = "Jostin Wilmer" };
+
+Course onlineCourse = new OnlineCourse { CourseId = 1, Title = "C# Cursos Online" };
+Course offlineCourse = new OfflineCourse { CourseId = 2, Title = "Python Offline Cursos" };
+Course hybridCourse = new HybridCourse { CourseId = 3, Title = "C#,C++ Hybrid Course" };
+
+onlineCourse.Subscribe(student);
+offlineCourse.Subscribe(student);
+hybridCourse.Subscribe(student);
